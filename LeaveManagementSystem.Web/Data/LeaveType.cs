4521251a4e9cf8453f53cad759f879e9c7645e07
mbaseski <mbasheski@hotmail.com>
2024-06-13 20:40:37 +0200

    Completed Leave Managment Module

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.CodeAnalysis.Classification;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName ="nvarchar(150)")]
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+
+    }
+}

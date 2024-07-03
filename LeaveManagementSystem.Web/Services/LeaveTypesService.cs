0e8ac7b4ff7ab4ea2b01de9769abc719a5f9f953
mbaseski <mbasheski@hotmail.com>
2024-07-03 22:59:37 +0200

    90. Adding the Service Layer (Contract and Implementation)

---
+++ LeaveManagementSystem.Web/Services/LeaveTypesService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using LeaveManagementSystem.Web.Data;
+using LeaveManagementSystem.Web.Models.LeaveTypes;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagementSystem.Web.Services;
+
+public class LeaveTypesService(ApplicationDbContext context, IMapper mapper)
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public async Task<List<LeaveTypeReadOnlyVM>> GetAll()
+    {
+        //data = SELECT * FROM LeavTypes
+        var data = await _context.LeaveTypes.ToListAsync();
+        //convert the datamodel into a view model using AutoMapper
+        var viewData = _mapper.Map<List<LeaveTypeReadOnlyVM>>(data);
+        return viewData;
+    }
+
+    public async Task<T?> Get<T>(int id) where T : class
+    {
+        var data = await _context.LeaveTypes.FirstOrDefaultAsync(x => x.Id == id);
+        if (data == null)
+        {
+            return null;
+        }
+        var viewData = _mapper.Map<T>(data);
+        return viewData;
+    }
+
+    public async Task Remove(int id)
+    {
+        var data = await _context.LeaveTypes.FirstOrDefaultAsync(x => x.Id == id);
+        if (data != null)
+        {
+            _context.Remove(data);
+            await _context.SaveChangesAsync();
+        }
+    }
+        
+}
